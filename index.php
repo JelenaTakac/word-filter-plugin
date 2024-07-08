a7e5edcd4759eea0ba238b9f55725b5b9305c83a
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-08 07:34:18 +0200

    Manually handlling admin form submit

--- index.php
+++ index.php
@@ -25,6 +25,29 @@ class OurWordFilterPlugin
     function __construct()
     {
         add_action('admin_menu', array($this, 'ourMenu'));
+        add_action('admin_init', array($this, 'ourSettings'));
+        if (get_option('plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic'));
+    }
+
+    function ourSettings()
+    {
+        add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+        register_setting('replacementFields', 'replacementText');
+        add_settings_field('replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+    }
+
+    function replacementFieldHTML()
+    { ?>
+        <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')) ?>">
+        <p class="description">Leave blank to simply remove the filtered words.</p>
+        <?php }
+        
+
+    function filterLogic($content)
+    {
+        $badWords = explode(',', get_option('plugin_words_to_filter')); // this return array
+        $badWordsTrimmed = array_map('trim', $badWords);
+        return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', '****')), $content);
     }
 
     function ourMenu()
@@ -38,20 +61,38 @@ class OurWordFilterPlugin
         add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
     }
 
-    function mainPageAssets() {
+    function mainPageAssets()
+    {
         wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__) . 'styles.css');
+    }
+
+    function handleForm()
+    {
+        if (wp_verify_nonce($_POST['ourNonce'], 'saveFilterWords') and current_user_can('manage_options')) {
+            update_option('plugin_words_to_filter', sanitize_text_field($_POST['plugin_words_to_filter'])); ?>
+            <div class="updated">
+                <p>Your filtered words were saved.</p>
+            </div>
+        <?php } else { ?>
+            <div class="error">
+                <p>Sorry, you do not have permission to perform that action.</p>
+            </div>
+        <?php }
     }
 
     function wordFilterPage()
     { ?>
         <div class="wrap">
             <h1>Word Filter</h1>
+            <?php if (isset($_POST['justsubmitted']) == "true") $this->handleForm(); ?>
             <form method="POST">
+                <input type="hidden" name="justsubmitted" value="true">
+                <?php wp_nonce_field('saveFilterWords', 'ourNonce') ?>
                 <label for="plugin_words_to_filter">
                     <p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content.</p>
                 </label>
                 <div class="word-filter__flex-container">
-                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"></textarea>
+                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"><?php echo esc_textarea(get_option('plugin_words_to_filter')); ?></textarea>
                 </div>
                 <input class="button button-primary" type="submit" name="submit" id="submit" value="Save Changes">
             </form>
@@ -60,7 +101,17 @@ class OurWordFilterPlugin
 
     function optionsSubPage()
     { ?>
-        hello sub menu page;
+        <div class="wrap">
+            <h1>Word Filter Options</h1>
+            <form action="options.php" method="POST">
+                <?php
+                    settings_errors();
+                    settings_fields('replacementFields');
+                    do_settings_sections('word-filter-options');
+                    submit_button();
+                ?>
+            </form>
+        </div>
 <?php }
 }
 
